20751083ac390fcc11fc6d47c34d6ff7489020e3
JohnMark13 <mark@soonami.io>
2022-06-02 10:14:51 +0100

    Remove unused config and update Guild ID to be Nation3 post testing

--- config.js
+++ config.js
@@ -1,16 +1,8 @@
 module.exports = {
     GUILD_IDS: {
-        "956551108061446244": {
-            repo: "nation3/nationcred-instance", 
-            branch: "activation-bot-test", 
-            discordBaseURL: "https://forum.nation3.org",
-            chainId: "1", 
-            tokenAddress: "0x333a4823466879eef910a04d473505da62142069"
-        },
         "690584551239581708": {
             repo: "nation3/nationcred-instance", 
             branch: "activation-bot-test", 
-            discordBaseURL: "https://forum.nation3.org",
             chainId: "1", 
             tokenAddress: "0x333a4823466879eef910a04d473505da62142069"
         }
